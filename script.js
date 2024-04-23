0391accc681ee14c3e516470468ee69475c13576
pepperrostami <pepper.rostami@gmail.com>
2024-04-23 18:22:52 -0400

    created basic framework for game

---
+++ script.js
@@ -0,0 +1,43 @@
+    let pet = {
+        name: "Bubzee",
+    };
+
+    function updatePetDisplay() {
+
+    }
+
+//Functions for tamagotchi statuses
+    document.getElementById("feed").addEventListener("click", function() {
+
+    });
+
+    document.getElementById("play").addEventListener("click", function() {
+
+    });
+
+    document.getElementById("medicine").addEventListener("click", function() {
+
+    });
+
+    document.getElementById("attention").addEventListener("click", function() {
+
+    });
+
+    document.getElementById("discipline").addEventListener("click", function() {
+
+    });
+
+    document.getElementById("health-meter").addEventListener("click", function() {
+
+    });
+
+//Functions for primary A,B,C buttons
+    document.getElementById("a").addEventListener("click", function() {
+
+    });
+    document.getElementById("b").addEventListener("click", function() {
+
+    });
+    document.getElementById("c").addEventListener("click", function() {
+
+    });
